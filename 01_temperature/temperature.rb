b1d679eebaf6d59db0349cf056033e00ba34941d
Shahmeer <shahmeer.a.khan@gmail.com>
2019-07-20 00:17:41 +0500

    Completed Tasks

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,15 @@
 #write your code here
+def ftoc(temperature)
+    if temprature == 32
+        return 0
+    else
+        return (temperature − 32.0) * (5 / 9)
+end
+
+def ctof(temprature)
+    if temperature == 0
+        return 32
+    else
+        return (temperature * (9.0 / 5.0)) + 32
+end
+
