a2186caa64c2a51ef7841be6737bd86afecc326b
Shahmeer <shahmeer.a.khan@gmail.com>
2019-07-20 00:29:39 +0500

    Completed

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,14 @@
 #write your code here
+def add(a,b)
+    return (a + b)
+end
+
+def subtract(a, b)
+    return(a - b)
+end
+
+def sum(a)
+    total = 0
+    a.each{|x| total += x}
+    return total
+end
